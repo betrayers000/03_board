38fe938013f9c65538aee7d492816f5b5379f475
jw <betrayers000@gmail.com>
2019-08-07 11:14:24 +0900

    add read

--- todos/urls.py
+++ todos/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # for Create
     path('new/', views.new),
     path('create/', views.create),
+    # for Read
+    path('', views.index)
 ]
--- todos/views.py
+++ todos/views.py
@@ -16,4 +16,14 @@ def create(request):
     todo.content = content
     todo.due_date = due_date    
     todo.save()
+
+    # todo = Todo(title=title, content=content, due_date=due_date)
+    # todo.save()
     return render(request, 'create.html')
+
+def index(request):
+    todos = Todo.objects.all()
+    context = {
+        'todos': todos,
+    }
+    return render(request, 'index.html', context)
